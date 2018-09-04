ad3d72d22c736e535a9f1b8918ee4404d7a69d89
59160089 <59160089@go.buu.ac.th>
2018-09-04 20:02:47 +0700

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import javax.swing.*;
+
 /**
  * Created by DT11ST on 9/4/2018.
  */
 public class Main {
-    
+    public static void main(String[] args) {
+        String input = JOptionPane.showInputDialog("ใส่ตัวเลข 1 ตัว","0");
+        JOptionPane.showMessageDialog(null,"intput : "+input,"title",JOptionPane.INFORMATION_MESSAGE);
+        int ans =JOptionPane.showConfirmDialog(null,"รักหรือเปล่า","ถามเธออะ",JOptionPane.YES_NO_OPTION);
+
+        if(ans==JOptionPane.YES_OPTION){
+            JOptionPane.showMessageDialog(null,"หรอ");
+        }else {
+            JOptionPane.showMessageDialog(null,"แล้วไป");
+        }
+    }
+
 }
